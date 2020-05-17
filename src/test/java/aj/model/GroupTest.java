4d45da0af29237e3cbb4e2b023eacb28abfa984c
PCz <mail.00.kontakt@gmail.com>
2020-05-17 12:42:24 +0200

    befor Jupiter test assertj

--- src/test/java/aj/model/GroupTest.java
+++ src/test/java/aj/model/GroupTest.java
@@ -1,5 +1,6 @@
 package aj.model;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
@@ -72,14 +73,15 @@ class GroupTest {
     }*/
 
     @Test
-    public void shouldNotAllowRemoveAdminUser() {
+    public void shouldNotAllowToRemoveAdmin() throws GroupOperationException {
         //given
         User user = new User("Adam", "Adamowicz", 25, Sex.MALE);
         Group group = new Group("Adamowe");
 
-        Executable executable = () -> group.addUser(user);
+        group.addUser(user);
 
         int expectedGroupSize = 1;
+        int initialGroupSize = group.getGroupSize();
 
         //when
         Executable executableRemove = () -> group.removeUser(user);
@@ -87,12 +89,37 @@ class GroupTest {
         //then
         GroupOperationException groupOperationException = assertThrows(GroupOperationException.class, executableRemove);
         assertEquals(CANNOT_REMOVE_ADMIN, groupOperationException.getMessage());
+        assertEquals(initialGroupSize, group.getGroupSize());
+        assertEquals(expectedGroupSize, group.getGroupSize());
 
 
     }
 
     @Test
+    @Disabled // wyłączamy, bo poniżej jest jupiter
     public void shouldRemoveUserCorrectly() throws GroupOperationException {
+        //given
+        User user = new User("Adam", "Adamowicz", 25, Sex.MALE);
+        User user2 = new User("Karol", "Adamowicz", 25, Sex.MALE);
+        Group group = new Group("Adamowe");
+
+        group.addUser(user);
+        group.addUser(user2);
+
+        int expectedGroupSize = 1;
+        int userNotPresent = -1;
+
+        //when
+        group.removeUser(user2);
+
+        //then
+        assertEquals(expectedGroupSize, group.getGroupSize());
+        assertEquals(user, group.getUsers().get(0));
+        assertEquals(userNotPresent, group.getUsers().indexOf(user2));
+    }
+
+    @Test
+    public void shouldRemoveUserCorrectlyJupiter() throws GroupOperationException {
         //given
         User user = new User("Adam", "Adamowicz", 25, Sex.MALE);
         User user2 = new User("Karol", "Adamowicz", 25, Sex.MALE);
@@ -118,12 +145,55 @@ class GroupTest {
         //given
         User user = new User("Adam", "Adamowicz", 25, Sex.MALE);
         Group group = new Group("Adamowe");
-        
+
         //when
         group.addUser(user);
 
         //then
         assertEquals(user, group.getAdmin());
     }
+
+    @Test
+    public void shouldChangeAdminCorrectly() throws GroupOperationException {
+        //given
+        User user = new User("Adam", "Adamowicz", 25, Sex.MALE);
+        User user2 = new User("Karol", "Adamowicz", 25, Sex.MALE);
+        Group group = new Group("Adamowe");
+
+        group.addUser(user);
+        group.addUser(user2);
+        User initialAdmin = group.getAdmin();
+
+        //when
+        group.changeGroupAdmin(user2);
+
+        //then
+        assertEquals(user2, group.getAdmin());
+        assertEquals(initialAdmin, user);
+
+    }
+    @Test
+    public void shouldNotAllowToChangeAdmin() throws GroupOperationException {
+        //given
+        User user = new User("Adam", "Adamowicz", 25, Sex.MALE);
+        User user2 = new User("Karol", "Adamowicz", 25, Sex.MALE);
+        Group group = new Group("Adamowe");
+
+        group.addUser(user);
+        group.addUser(user2);
+        User initialAdmin = group.getAdmin();
+
+        //when
+        Executable executableChangeGroup = () -> group.changeGroupAdmin(user);
+
+        //then
+        GroupOperationException groupOperationException = assertThrows(GroupOperationException.class, executableChangeGroup);
+        assertEquals(NEW_ADMIN_MUST_BE_DIFFERENT_USER, groupOperationException.getMessage());
+
+        assertEquals(user, group.getAdmin());
+        assertEquals(initialAdmin, user);
+
+    }
+
 
 }
